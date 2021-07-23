275cfce252c42a04dccedefa17119f5f3bb73624
Your Name <you@example.com>
2021-07-23 20:01:44 -0300

    first commit

---
+++ Script/Cadastro.js
@@ -0,0 +1,24 @@
+
+
+
+function cad(){
+	let email = document.getElementById('ema').value;
+	let sen1 = document.getElementById('sen').value;
+	let sen2 = document.getElementById('send').value;
+
+		if(email != ""  &&  sen1 != "" && sen2 != ""){
+
+			alert("Informaçoes OK");
+
+
+
+		}else{
+
+			alert("Verja se esta tudo corrertto");
+
+
+		}	
+
+
+
+}
---
+++ Script/Script.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+function cadastro(){
+
+const m =document.getElementById('email').value;
+const s =document.getElementById('senha').value;
+
+	if(m != "" &&  s != ""){
+
+	
+
+	}else{
+
+		alert('PRENCHER TODAS AS INFORMAÇOES');
+
+	}
+	
+	
+
+
+
+	
+
+		
+
+
+
+
+}
+
